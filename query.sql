d12244c92fe33026e14ce92f40789616088be8f0
Debangan Bhattacharyya <112092614+debangan03@users.noreply.github.com>
2025-04-09 19:26:19 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,11 @@
 SHOW DATABASES;
+
+
+DELETE from credit.DATABASECHANGELOG
+where id='add-delivery-dispatch-columns-to-invoice-history';
+ 
+ALTER TABLE credit.invoice_history
+DROP COLUMN delivery_date_time;
+ 
+ALTER TABLE credit.invoice_history
+DROP COLUMN estimated_dispatch_date;
